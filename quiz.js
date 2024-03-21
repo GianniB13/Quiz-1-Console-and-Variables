a4ab73fc9722e3b560529910def651d2201b1761
GianniB13 <159183504+GianniB13@users.noreply.github.com>
2024-03-21 13:31:32 -0400

    Temperature

--- quiz.js
+++ quiz.js
@@ -1,6 +1,9 @@
 const prompt = require('prompt-sync')();
 
 //Program 1: Temperature Converter
+let Fahr = prompt("Input the temperature in fahrenheit");
+console.log(Fahr);
+
 
 //Program 2: Grade Calculator
 
